419ba545e707fc808a1f63973d25415b4af6b5b3
bkrishna <b.krishna@procreate.co.in>
2022-06-05 09:08:24 +0530

    ButtonBox is implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import Button from "./components/Button";
+import ButtonBox from "./components/ButtonBox";
+import Screen from "./components/Screen";
 import Wrapper from "./components/Wrapper";
+
+const btnValues = [
+  ["C","+-","%","/"],
+  [7,8,9,"x"],
+  [4,5,6,"-"],
+  [1,2,3,"+"],
+  [0,".","="]
+]
 
 function App() {
   return (
     <div>
         <Wrapper>
-          <h1>App</h1>
+          <Screen />
+         <ButtonBox>
+          {btnValues.flat().map((btn,i) => (
+            <Button 
+              value={btn}
+              key={i}
+            />
+          ))}
+         </ButtonBox>
         </Wrapper>
     </div>
   );
---
+++ src/components/Screen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Screen = () => {
+  return (
+    <div className='screen'>
+        085854
+    </div>
+  )
+}
+
+export default Screen
